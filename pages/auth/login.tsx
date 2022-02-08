b3ad998f52c5a4f6b8d82700eba6505caaaf5195
Rajdeep <rajdipgupta019@gmail>
2022-02-08 15:13:16 +0530

    Meme Media Initialized V-1.1

---
+++ pages/auth/login.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const login: React.FC = () => {
+  return <div></div>;
+};
+
+export default login;
---
+++ pages/components/Feed/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main: React.FC = () => {
+  return <div>Hello There Again</div>;
+};
+
+export default Main;
