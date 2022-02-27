5bf994b4e993f7601ca25a360513a5c07f57aedb
Rajdeep <rajdipgupta019@gmail>
2022-02-27 10:40:40 +0530

    News Section Added

---
+++ interfaces/meme-news.d.ts
@@ -0,0 +1,11 @@
+export default interface MemeNews {
+    dateLastCrawled: string,
+    displayUrl: string
+    id: string,
+    name : string,
+    url: string,
+    isFamilyFriendly : boolean,
+    isNavigational: boolean,
+    language: string,
+    snippet: string
+}
--- pages/components/Feed/RightNewsBar/RightNewsBar.tsx
+++ pages/components/Feed/RightNewsBar/RightNewsBar.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import SingleNew from './SingleNew';
+import React, { useEffect, useState } from 'react';
+import SingleNew from './SingleNews';
+import { template } from '../../../../helpers/template';
+import MemeNews from '../../../../interfaces/meme-news';
+import Moment from 'react-moment';
 
 const RightNewsBar: React.FC = () => {
+
+  const {templateString}  = template;
+  const [memeNews, setMemeNews] = useState<Array<MemeNews>>([])
+
+  const handleMemeNews = async () => {
+      const res = await fetch(`${templateString}/meme-news`)
+      const newsData = await res.json();
+      setMemeNews(newsData?.webPages?.value)
+  }
+
+
+  useEffect(() => {
+    handleMemeNews(); 
+  }, [])
+
+
   return (
     <div className="w-72 bg-[#0d0d0d] mx-auto mt-5 rounded-xl py-4 text-gray-100 sticky top-5">
       <div className=" flex items-center justify-between px-4">
-        <h2 className="font-semibold">Meme News </h2>
+        <h2 className="font-semibold">Top Meme News </h2>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5 cursor-pointer"
@@ -19,7 +38,16 @@ const RightNewsBar: React.FC = () => {
           />
         </svg>
       </div>
-      <SingleNew />
+      {memeNews?.map((memeNews: MemeNews) => {
+        return(
+          <SingleNew 
+          name ={memeNews.name}
+          url = {memeNews.url}
+          snippet = {memeNews.snippet}
+          language = {memeNews.language}
+          />
+        )
+      })}
     </div>
   );
 };
